1b3904ca581dc2ed80c1fdf665a7af10b858e83a
Chuck McManus <107281140+mcmanuscw@users.noreply.github.com>
2022-07-12 15:34:52 -0400

    Tidying up the code with comments and further refinements

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,13 @@
 # Dependencies
+from optparse import Values
 import os
 import csv
 
-from numpy import average, double
+from numpy import average, double, int0
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-# Reading using CSV module
+
 
 # --------------------Initialize Lists-------------
 
@@ -17,6 +18,9 @@ Investments = []
 diff_list = []
 
 # --------------------------------------------------
+
+
+# Reading using CSV module
 
 with open(csvpath) as csvfile:
 
@@ -32,48 +36,36 @@ with open(csvpath) as csvfile:
     # # Read each row of data after the header
     for index, row in enumerate(csvreader):
         # print(index, row[1]) #Prints element 1 aka first column
-        Investments.append(float(row[1]))
+        Investments.append(int(row[1]))
+        Investments.append(row[0],int(row[1]))
 
     # print inital list
-    # print(Investments)
+    print(Investments)
  
     for x, y in zip(Investments[0::], Investments[1::]):
        diff_list.append(y-x) 
 
     # print difference list
-    #  print(diff_list)
+    #print(diff_list)
 
-print(average(diff_list))
-print(min(diff_list))
-print(max(diff_list))
-print(len(Investments))
+ 
+RoundedAverage = round(average(diff_list),2)
 
+print("")
+print("Financial Analysis")
+print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+print("")
+print(f"Total # Months: {len(Investments)}")
+print(f"Net Total PnL:  ${sum(Investments)}")
+print(f"Average Change: ${RoundedAverage}")
+print(f"Average Change: (${min(diff_list)})")
+print(f"Greatest Increaes in Profits: $({max(diff_list)})")
 
-
-#     # Assignment Instructions ref 03-python 12-Stu-CencusZip
-# # Your task is to create a Python script that analyzes the records to calculate each of the following values:
-
-# # Lists t store data
-# NumberOfMonths = []
-# NetProfitLoss = []
-# GreatestIncrease = []
-# GreatestDecrease = []
-# functin
-
-# The total number of months included in the dataset
-# cwmcomments >>>>> month number of the latest (max) date and subtract the month number of the earliest (min) date
+ # Write to File
+    L_Desc = ["Total number of months:", "Net Total PnL:", "Average Change:", "Greatst Increaes in Profits:", "Greatest Decrease in Profits:"]
+    L_Values = [len(Investments), sum(Investments), average(diff_list), max(diff_list), min(diff_list)]
 
 
-
-
-        
-        # The net total amount of "Profit/Losses" over the entire period
-
-        # The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-        # The greatest increase in profits (date and amount) over the entire period
-
-        # The greatest decrease in profits (date and amount) over the entire period
 # Stagging
 
 # git add .
