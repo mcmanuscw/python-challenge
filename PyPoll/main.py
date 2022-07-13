7c52255215ed5e2691e295eff0175e37dad6c8ac
Chuck McManus <107281140+mcmanuscw@users.noreply.github.com>
2022-07-13 15:37:51 -0400

    Hart time with this one

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,18 @@
 # Dependencies
+from calendar import c
+from locale import currency
 from optparse import Values
 import os
 import csv
-from socket import RDS_RDMA_DONTWAIT
+from typing import Counter
 
 from numpy import average, double, int0
 
 # --------------------Initialize Lists-------------
 
-# # Initialize List for votes
- votes = []
-
-# # Initialize Investment Change (Difference List
-# diff_list = []
+# # Initialize Lists
+votes = []
+candidates = []
 
 # --------------------------------------------------
 
@@ -27,39 +27,65 @@ with open(csvpath) as csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     # # Read each row of data after the header and read them into a list
     for row in csvreader:
-        votes.append(0::)
-        
-        if row[2] = "Raymon Anthony Doane"
-            print(row)
+        votes.append(row[2])
+
+    for row in csvreader:
+        candidates.append(row[1])
+
+print(candidates)
+
+# Candidates = [votes[2]]
+# CandidatesUnique = []
+    
+# for element in Candidates:
+#     if element not in Candidates:
+#         CandidatesUnique.append
+
+# print("Unique Candidates",Candidates)
 
 
-#     # print inital list
-#     #print(Investments)
- 
-#     for x, y in zip(Investments[0::], Investments[1::]):
-#        diff_list.append(y-x) 
 
-#     # print difference list
-#     #print(diff_list)
+            
+# print(Counter)
 
- 
-#     RoundedAverage = round(average(diff_list),2)
+TotalVotes = (len(votes))
 
-# # Print Results to Terminal
+# CWM Notes
+    # List to get unique values candidates
+        # Find unique candidates
+        #get count of votes, grouped by candidates; divide by total # votes to get Pct
+    # Create list of candidates and percentage won
+    # Determine the winner by max(percent_won)
 
-# print("")
-# print("Financial Analysis")
-# print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
-# print("")
-# print(f"Total # Months: {len(Investments)}")
-# print(f"Net Total PnL:  ${sum(Investments)}")
-# print(f"Average Change: ${RoundedAverage}")
-# print(f"Greatest Decrease: (${min(diff_list)})")
-# print(f"Greatest Increase in Profits: $({max(diff_list)})")
+    #Print out result set
 
-# # I ran out of time and couldn't successfully figure out how to write the results to an output file I am 
-# print("hello")
+
+
+
+
+# Assignment Requirements:
+
+        # The total number of votes cast
+        # A complete list of candidates who received votes
+        # The percentage of votes each candidate won
+        # The total number of votes each candidate won
+        # The winner of the election based on popular vote
+        # Your analysis should align with the following results:
+
+        # Election Results
+        # -------------------------
+        # Total Votes: 369711
+        # -------------------------
+        # Charles Casper Stockham: 23.049% (85213)
+        # Diana DeGette: 73.812% (272892)
+        # Raymon Anthony Doane: 3.139% (11606)
+        # -------------------------
+        # Winner: Diana DeGette
+        # -------------------------
+        # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+
